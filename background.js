79892f1c4ef47fe72b29e16525627b3512968ec0
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2021-08-28 16:13:34 +0530

    Added chrome extension
    Added chrome extension made using JS, to replace all occurrences of "shark" on a web page with it's emoji version.

---
+++ background.js
@@ -0,0 +1,15 @@
+function changeInnerHTML() {
+    let text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
+    for (let i=0; i<text.length; ++i) {
+        if (text[i].innerHTML.includes('shark')) {
+            text[i].innerHTML = text[i].innerHTML.replace('shark', 	'&#x1f988');
+        }
+    }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: changeInnerHTML
+    });
+});
