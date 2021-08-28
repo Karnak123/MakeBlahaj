ba6297fac2eb14ae4d2fa604f7048b2bbc92d8ab
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2021-08-28 22:56:05 +0530

    Minor changes
    Used regex to replace globally and case insensitively

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
 function changeInnerHTML() {
     let text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
     for (let i=0; i<text.length; ++i) {
-        if (text[i].innerHTML.includes('shark')) {
-            text[i].innerHTML = text[i].innerHTML.replace('shark', 	'&#x1f988');
-        }
+        text[i].innerHTML = text[i].innerHTML.replace(/shark/gi, '&#x1f988');
     }
 }
 
